9d8282cf9526ade924e1b3b5ec3ef206ac7efb51
abjshawty <kouadiobhegnino@gmail.com>
2025-10-19 04:52:41 +0000

    MIME (vite.config)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/station_gaming',
+  base: '/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
